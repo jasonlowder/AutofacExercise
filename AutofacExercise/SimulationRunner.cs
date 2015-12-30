27de93e7afa2a1614885a1a8fcaa1d0692c9871e
Jason Lowder <jlowder@accesssoftek.com>
2015-12-30 09:09:20 -0700

    Create solution and baseline

---
+++ AutofacExercise/SimulationRunner.cs
@@ -0,0 +1,11 @@
+﻿namespace AutofacExercise
+{
+    class SimulationRunner
+    {
+        static void Main(string[] args)
+        {
+            var simulator = new Magic8BallSimulator();
+            simulator.Run();
+        }
+    }
+}
